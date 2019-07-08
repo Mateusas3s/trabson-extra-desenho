d7e0d9be46b580025909b5ec989b2ff4d2e2e4ab
Vitor Leal <vitinho2201@gmail.com>
2019-07-08 19:27:11 -0300

    Init Combo screen
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Mateus Sousa <mateusaugusto-2009@hotmail.com>

---
+++ App-trabson-extra/src/screens/screenCombo1/ScreenCombo1.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { 
+    StyleSheet,
+    View,
+    Text,
+    Image
+} from 'react-native';
+import Bebida1 from '..//screenBebida1/ScreenBebida1';
+import Comida1 from '../screenComida1/ScreenComida1';
+
+const imgComida1 = require('../../../assets/comida1.jpeg');
+
+export default class Combo1 extends React.Component {
+  render() {
+    return (
+      <View>
+        <Bebida1/>
+        <Comida1/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    cabecalho: {
+      flexDirection: 'row',
+      marginTop: 20
+    },
+    txtTitulo: {
+      fontSize: 30,
+      color: '#b9c941',
+      marginLeft: 10,
+      marginTop: 20
+    },
+    detalhe: {
+      padding: 20,
+      marginTop: 10
+    },
+    txtDetalheTitulo:{
+        fontSize: 26,
+        color: '#222222',
+        marginLeft: 10,
+        marginTop: 20
+    },
+    txtDetalhe: {
+      fontSize: 20,
+      color: '#222222',
+      marginLeft: 10,
+      marginTop: 20
+    },
+    imgMenu: {
+        margin: 15,
+        width: 120,
+        height: 120,
+      }
+});
