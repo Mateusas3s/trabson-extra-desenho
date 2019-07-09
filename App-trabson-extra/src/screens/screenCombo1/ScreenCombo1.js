e8d2ca8c0a6f50937405ec58a197e29bd6eabe05
mateusas3s <mateusaugusto-2009@hotmail.com>
2019-07-08 21:01:34 -0300

    Adicionando telas do código
    Signed-off-by: mateusas3s <mateusaugusto-2009@hotmail.com>
    Co-authored-by: Vitor Leal  <vitor_leal2201@hotmail.com>

--- App-trabson-extra/src/screens/screenCombo1/ScreenCombo1.js
+++ App-trabson-extra/src/screens/screenCombo1/ScreenCombo1.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { 
-    StyleSheet,
-    View,
-    Text,
-    Image,
     ScrollView,
     StatusBar,
 } from 'react-native';
@@ -11,7 +7,6 @@ import Bebida1 from '../../snacks/bebida1/Bebida1';
 import Comida1 from '../../snacks/comida1/Comida1';
 import BuyButton from '../../components/buyButton';
 
-const imgComida1 = require('../../../assets/comida1.jpeg');
 import BarraNavegacao from '../../barNavigation/BarraNavegacao';
 
 export default class ScreenCombo1 extends React.Component {
@@ -28,36 +23,4 @@ export default class ScreenCombo1 extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-    cabecalho: {
-      flexDirection: 'row',
-      marginTop: 20
-    },
-    txtTitulo: {
-      fontSize: 30,
-      color: '#b9c941',
-      marginLeft: 10,
-      marginTop: 20
-    },
-    detalhe: {
-      padding: 20,
-      marginTop: 10
-    },
-    txtDetalheTitulo:{
-        fontSize: 26,
-        color: '#222222',
-        marginLeft: 10,
-        marginTop: 20
-    },
-    txtDetalhe: {
-      fontSize: 20,
-      color: '#222222',
-      marginLeft: 10,
-      marginTop: 20
-    },
-    imgMenu: {
-        margin: 15,
-        width: 120,
-        height: 120,
-      }
-});
+
